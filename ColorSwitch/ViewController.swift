e3bad9dec9c2f9abb60d0c2720f5e4af48ed1cd8
Ben Wilcox <reccanti@gmail.com>
2017-02-06 00:05:00 -0500

    Intial Commit

--- ColorSwitch/ViewController.swift
+++ ColorSwitch/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var swatch: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    /**
+     * a function that updates the color of 
+     * the color swatch
+     */
+    @IBAction func updateSwatch(_ sender: UISwitch) {
+        let redVal:Float = redSwitch.isOn ? 1.0 : 0.0
+        let greenVal:Float = greenSwitch.isOn ? 1.0 : 0.0
+        let blueVal:Float = blueSwitch.isOn ? 1.0 : 0.0
+        swatch.backgroundColor = UIColor(colorLiteralRed: redVal, green: greenVal, blue: blueVal, alpha: 1.0)
+    }
 }
 
